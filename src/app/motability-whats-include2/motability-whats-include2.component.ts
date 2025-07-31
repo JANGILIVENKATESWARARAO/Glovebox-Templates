3be919e96e4d407bd1da87ed92f689b5e77da2dd
c.pradhan <c.pradhan@C-PRADHAN-WS>
2025-07-31 08:21:51 +0100

    Tire-1 Motability 2 what's Included section added

---
+++ src/app/motability-whats-include2/motability-whats-include2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motability-whats-include2',
+  standalone: true,
+  imports: [],
+  templateUrl: './motability-whats-include2.component.html',
+  styleUrl: './motability-whats-include2.component.css'
+})
+export class MotabilityWhatsInclude2Component {
+
+}
