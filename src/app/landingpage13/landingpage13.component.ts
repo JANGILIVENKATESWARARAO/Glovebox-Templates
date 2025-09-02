c8fa3476c6a56b286bb7edcd0f42fa16a8913e57
p.polineni <p.polineni@P-POLINENI-WS>
2025-09-02 08:13:01 +0100

    Template-13 Responsive

--- src/app/landingpage13/landingpage13.component.ts
+++ src/app/landingpage13/landingpage13.component.ts
@@ -7,11 +7,12 @@ import { AboutParkway4Component } from '../about-parkway4/about-parkway4.compone
 import { Footer2Component } from '../footer2/footer2.component';
 import { Header4Component } from '../header4/header4.component';
 import { HomepageHero1Component } from '../homepage-hero1/homepage-hero1.component';
+import { Header3Component } from '../header3/header3.component';
 
 @Component({
   selector: 'app-landingpage13',
   standalone: true,
-  imports: [Header4Component,HomepageHero1Component,PerfectPartner1Component,UsedCars1Component,Motability3Component,VechicalsServices3Component,AboutParkway4Component,Footer2Component],
+  imports: [Header3Component,HomepageHero1Component,PerfectPartner1Component,UsedCars1Component,Motability3Component,VechicalsServices3Component,AboutParkway4Component,Footer2Component],
   templateUrl: './landingpage13.component.html',
   styleUrl: './landingpage13.component.css'
 })
