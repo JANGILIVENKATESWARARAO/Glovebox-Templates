dbd83a98489c5cc06f675a44ed0d1aefc790e112
p.polineni <p.polineni@P-POLINENI-WS>
2025-09-09 11:50:39 +0100

    Template-14 Changes and responsive

--- src/app/homepage-hero2/homepage-hero2.component.ts
+++ src/app/homepage-hero2/homepage-hero2.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UsedCars1Component } from '../used-cars1/used-cars1.component';
 
 @Component({
   selector: 'app-homepage-hero2',
   standalone: true,
-  imports: [],
+  imports: [UsedCars1Component],
   templateUrl: './homepage-hero2.component.html',
   styleUrl: './homepage-hero2.component.css'
 })
